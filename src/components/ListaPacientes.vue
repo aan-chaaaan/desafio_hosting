090e55e09d7821d098c000ab0882415229d24ae0
angela <angela.valdebenito.v@gmail.com>
2021-10-02 19:59:23 -0300

    commit

--- src/components/ListaPacientes.vue
+++ src/components/ListaPacientes.vue
@@ -9,12 +9,24 @@
         <th scope="col">Acciones</th>
       </tr>
     </thead>
-    <tbody v-for="paciente in $store.state.pacientes" :key="paciente.id">
+    <!-- <tbody v-for="paciente in $store.state.pacientes" :key="paciente.id">
       <tr>
         <td>{{ paciente.nombre }}</td>
         <td>{{ paciente.apellido }}</td>
         <td>{{ paciente.edad }}</td>
         <td>{{ paciente.prevision }}</td>
+        <td class="text-center">
+          <button class="btn btn-outline-info m-1">Eliminar</button>
+          <button class="btn btn-outline-info m-1">Editar</button>
+        </td>
+      </tr>
+    </tbody> -->
+    <tbody>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
         <td class="text-center">
           <button class="btn btn-outline-info m-1">Eliminar</button>
           <button class="btn btn-outline-info m-1">Editar</button>
@@ -39,7 +51,7 @@ export default {
         collection.forEach((document) => {
           this.pacientes.push({ id: document.id, ...document.data() });
         });
-      });
+    });
   },
 };
 </script>
