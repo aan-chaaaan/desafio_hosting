c9293a26ee12255a3401061adcd3814c2aabf2e6
angela <77757791+aan-chaaaan@users.noreply.github.com>
2021-10-03 17:23:36 -0300

    Delete firebaseConfig.js

--- firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAFQp6eCJzxSB3Er94LrRb_0e6gYJ3ROAA",
-  authDomain: "aut-crud.firebaseapp.com",
-  projectId: "aut-crud",
-  storageBucket: "aut-crud.appspot.com",
-  messagingSenderId: "1091341855828",
-  appId: "1:1091341855828:web:3bcbaa0c0cbe7ac8ebd1c1",
-  measurementId: "G-LPK0353T8R",
-};
